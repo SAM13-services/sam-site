fce6e2cd33fe67862e81b7fef7ea13f4d8228027
Lenny Bismuth <lennybismuth@lenny_macbook.local>
2026-03-19 12:21:54 +0100

    Revert to dotLottie player

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,16 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    exclude: ['@lottiefiles/dotlottie-react'],
+  },
   build: {
     rollupOptions: {
       output: {
         manualChunks: {
           'vendor-react': ['react', 'react-dom', 'react-router-dom'],
           'vendor-three': ['three', '@react-three/fiber', '@react-three/drei', '@react-spring/three'],
-          'vendor-lottie': ['lottie-react'],
+          'vendor-lottie': ['@lottiefiles/dotlottie-react'],
         },
       },
     },
